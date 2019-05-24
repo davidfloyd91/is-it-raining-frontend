30fd4b63989588ecad9aa6d37f1f73402ec6a125
davidfloyd91 <davidfloyd91@gmail.com>
2019-05-24 11:26:54 -0400

    geocoding in place

--- index.js
+++ index.js
@@ -2,22 +2,47 @@ document.addEventListener('DOMContentLoaded', e => {
   const rainDiv = document.querySelector('#rain-div');
   const url = 'http://is-it-raining-env.mqxjxhgsyd.us-east-1.elasticbeanstalk.com';
 
-  let minutelyForecast, isItRaining, precision;
-
-  // you need to grab these somehow
-  const lat = 41.584;
-  const lng = -43.681;
+  let minutelyForecast, isItRaining, precision, zip, coords, lat, lng;
 
   document.addEventListener('click', e => {
-    if (e.target.id === 'rain-button' && lat && lng) {
-      fetch(url + `/weather/${lat},${lng}`)
-      .then(r => r.json())
-      .then(res => setIsItRaining(res));
+    if (e.target.id === 'rain-button') {
+      if (lat && lng) {
+        fetch(url + `/weather/${lat},${lng}`)
+        .then(r => r.json())
+        .then(res => setIsItRaining(res));
+      } else {
+        alert('Please enter a five-digit zip code.');
+      };
     };
   });
 
+  document.addEventListener('input', e => {
+    if (e.target.id === 'zip') {
+      zip = e.target.value;
+    };
+  });
+
+  document.addEventListener('submit', e => {
+    e.preventDefault();
+    if (e.target.id === 'zip-form') {
+      if (!/^[0-9]{5}$/.test(zip) || !zip) {
+        alert('Please enter a five-digit zip code.');
+      } else {
+        fetch(url + `/location/${zip}`)
+        .then(r => r.json())
+        .then(res => getCoords(res));
+      };
+    };
+  });
+
+  const getCoords = res => {
+    coords = res.results[0].geometry.location;
+    lat = coords.lat.toFixed(3);
+    lng = coords.lng.toFixed(3);
+  };
+
   const rainArr = [
-    'It\'s probably not going to rain (less than 10 percent probability).',
+    'It\'s probably not gonna rain (less than 10 percent probability).',
     'It could rain (10 to 50 percent probability).',
     'It\'s probably gonna rain (greater than 50 percent probability).',
     'It\'s probably raining now (greater than 90% probability).'
