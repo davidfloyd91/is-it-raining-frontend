c2aa45debcd4171e67cdb4afb8a74e17c2b901dd
davidfloyd91 <davidfloyd91@gmail.com>
2019-05-28 22:58:32 -0400

    progress - need to work out ui flow

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 document.addEventListener('DOMContentLoaded', e => {
+  let coords, isItRaining, lat, lng, minutelyForecast, precision, zip;
+
   const rainDiv = document.querySelector('#rain-div');
+  const zipButton = document.querySelector('#zip-button');
+  const zipDisplay = document.querySelector('#zip-display');
+  const zipDisplayDiv = document.querySelector('#zip-display-div');
+  const zipForm = document.querySelector('#zip-form');
+
   const url = 'http://is-it-raining-env.mqxjxhgsyd.us-east-1.elasticbeanstalk.com';
 
-  let coords, isItRaining, lat, lng, minutelyForecast, precision, zip;
+  chrome.storage.local.get(['zip', 'lat', 'lng'], (result) => {
+    if (result.zip && zipDisplay) {
+      zip = result.zip;
+      zipDisplay.innerHTML += zip;
+      zipForm.style.display = 'none';
+    } else if (zipDisplayDiv) {
+      zipDisplayDiv.style.display = 'none';
+    };
+
+    if (result.lat && result.lng) {
+      lat = result.lat;
+      lng = result.lng;
+
+      getWeather();
+    };
+  });
 
   document.addEventListener('click', (e) => {
     if (e.target.id === 'rain-button') {
       if (lat && lng) {
-        fetch(url + `/weather/${lat},${lng}`)
-        .then((r) => r.json())
-        .then((res) => setIsItRaining(res));
+        getWeather();
       } else {
         alert('Please enter a five-digit zip code.');
+      };
+    } else if (e.target.id === 'zip-button') {
+      chrome.storage.local.remove('zip');
+      if (zipForm) {
+        zipForm.style.display = '';
       };
     };
   });
@@ -29,11 +54,7 @@ document.addEventListener('DOMContentLoaded', e => {
       if (!/^[0-9]{5}$/.test(zip) || !zip) {
         alert('Please enter a five-digit zip code.');
       } else {
-        chrome.storage.local.set({zip: zip}, () => {
-          chrome.storage.local.get(['zip'], (result) => {
-            alert('Zip currently is ' + result.zip);
-          });
-        });
+        chrome.storage.local.set({zip: zip});
 
         fetch(url + `/location/${zip}`)
         .then(r => r.json())
@@ -46,6 +67,16 @@ document.addEventListener('DOMContentLoaded', e => {
     coords = res.results[0].geometry.location;
     lat = coords.lat.toFixed(3);
     lng = coords.lng.toFixed(3);
+
+    chrome.storage.local.set({lat: lat, lng: lng});
+
+    getWeather();
+  };
+
+  const getWeather = () => {
+    fetch(url + `/weather/${lat},${lng}`)
+    .then((r) => r.json())
+    .then((res) => setIsItRaining(res));
   };
 
   const rainArr = [
